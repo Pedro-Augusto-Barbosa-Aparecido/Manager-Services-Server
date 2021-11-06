c6f34c7ba588ea959972e9d56f99f01c1a20f242
Pedro Augusto <pedro007augustobarbosa@gmail.com>
2021-11-05 21:41:56 -0300

    Implementando os outros models

--- databases/models.py
+++ databases/models.py
@@ -1,3 +1,192 @@
 from django.db import models
 
-# Create your models here.
+from settings.models import Account, BranchOffice, BusinessUnit, CostOwner, Department, DetailBusinessUnit, JobRole, Project, ServiceLevel, ServiceLocation, ServiceName, ServiceType, SubBusinessUnit, Supplier
+
+class OutsourcingDatabase(models.Model):
+    STATUS_ENTERED = "e"
+    STATUS_OPEN = "op"
+    STATUS_ON_LEAVE = "ol"
+    STATUS_RELEASED = "r"
+    STATUS_CANCELED = "c"
+    STATUS_HOLD = "h"
+    STATUS_CHOICES = (
+        (STATUS_ENTERED, "Active"),
+        (STATUS_OPEN, "Open"),
+        (STATUS_ON_LEAVE, "On leave"),
+        (STATUS_RELEASED, "Inactive"),
+        (STATUS_CANCELED, "Canceled"),
+        (STATUS_HOLD, "Hold"),
+    )
+
+    OUT_BY_LEVEL = "l"
+    OUT_BY_ITEM = "t"
+
+    CHOICES_OUT = (
+        (OUT_BY_LEVEL, "By level"),
+        (OUT_BY_ITEM, "By item"),
+    )
+
+    outsourcing_type = models.CharField(max_length=1, blank=False, null=False, choices=CHOICES_OUT)
+    outsourcing_id = models.CharField(max_length=10, blank=False, null=False, verbose_name="Non-Employee ID")
+
+    legacy_sm = models.CharField(max_length=8, blank=False, null=False, verbose_name="Legacy SRN")
+
+    status = models.CharField(max_length=2, blank=False, null=False, choices=STATUS_CHOICES)
+
+    tl_id = models.CharField(max_length=10, blank=False, null=False, verbose_name="TL ID")
+    tl_name = models.CharField(max_length=40, blank=False, null=False, verbose_name="TL Name")
+    is_tl = models.BooleanField(default=False, verbose_name="Is Team Leader")
+
+    account = models.ForeignKey(Account, blank=False, null=False, on_delete=models.PROTECT)
+    business_unit = models.ForeignKey(BusinessUnit, blank=False, null=False, on_delete=models.PROTECT)
+    supplier = models.ForeignKey(Supplier, blank=False, null=False, on_delete=models.PROTECT)
+    project = models.ForeignKey(Project, blank=False, null=False, on_delete=models.PROTECT)
+    cost_owner = models.ForeignKey(CostOwner, blank=False, null=False, on_delete=models.PROTECT)
+    branch_office = models.ForeignKey(BranchOffice, on_delete=models.PROTECT, null=True, blank=True)
+    department = models.ForeignKey(Department, on_delete=models.PROTECT, null=True)
+    service_type = models.ForeignKey(ServiceType, on_delete=models.PROTECT, null=True, blank=True)
+    service_name = models.ForeignKey(ServiceName, on_delete=models.PROTECT, null=True, blank=True)
+    service_level = models.ForeignKey(ServiceLevel, on_delete=models.PROTECT, null=True, blank=True)
+    service_location = models.ForeignKey(ServiceLocation, on_delete=models.PROTECT, null=True, blank=True)
+
+    start_date = models.DateField(verbose_name="Start date", blank=True, null=True)
+    actual_release_date = models.DateField(verbose_name="Release date", null=True, blank=True)
+    planned_start_date = models.DateField(verbose_name="Planned start date", null=True, blank=True)
+    planned_release_date = models.DateField(verbose_name="Planned release date", blank=True, null=True)
+
+    laptop = models.PositiveSmallIntegerField(default=0)
+    epi = models.PositiveSmallIntegerField(default=0, verbose_name="EPI")
+    smartphone = models.PositiveSmallIntegerField(default=0)
+    car = models.PositiveSmallIntegerField(default=0)
+    other = models.CharField(max_length=255, blank=True, null=True)
+
+    huawei_responsible_name = models.CharField(max_length=255, verbose_name="Service Responsible", null=True, blank=True)
+    huawei_responsible_id = models.CharField(max_length=255, verbose_name="Service Responsible ID", null=True, blank=True)
+    huawei_responsible_email = models.EmailField(verbose_name="Service Responsible email", null=True, blank=True)
+
+
+    remarks = models.CharField(max_length=80, blank=False, null=False)
+
+    class Meta:
+        verbose_name = "Outsourcing"
+        verbose_name_plural = "Outsourcing"
+
+        ordering = ("legacy_sm", "outsourcing_id")
+
+    def __str__(self) -> str:
+        return f"Non-Employee ID: {self.outsourcing_id}"
+
+class HuaweiDatabase(models.Model):
+    STATUS_ACTIVE = "a"
+    STATUS_INACTIVE = "i"
+    STATUS_CANCELED = 'c'
+    STATUS_CHOICES = (
+        (STATUS_ACTIVE, "Active"),
+        (STATUS_INACTIVE, "Inactive"),
+        (STATUS_CANCELED, 'Canceled'),
+    )
+
+    GENDER_FEMALE = "F"
+    GENDER_MALE = "M"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+    )
+
+    ORIGIN_CHN = "chn"
+    ORIGIN_LOCAL = "local"
+
+    ORIGIN_CHOICES = (
+        (ORIGIN_LOCAL, "China"),
+        (ORIGIN_CHN, "Local"),
+    )
+
+    employee_id = models.CharField(max_length=80, unique=True, verbose_name="Huawei ID")
+    employee_name = models.CharField(max_length=80, verbose_name="Name")
+
+    employee_email = models.EmailField(null=True, blank=True, verbose_name="Email")
+
+    start_date = models.DateField(verbose_name="Start date", null=True, blank=True) 
+    release_date = models.DateField(verbose_name="Release date", null=True, blank=True)
+    planned_start_date = models.DateField(verbose_name="Planned start date", null=True, blank=True)
+    planned_release_date = models.DateField(verbose_name="Planned release date", null=True, blank=True)
+
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default=STATUS_ACTIVE)
+    nationality = models.CharField(max_length=5, choices=ORIGIN_CHOICES)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+
+    base_location = models.ForeignKey(ServiceLocation, on_delete=models.PROTECT)
+    department = models.ForeignKey(Department, on_delete=models.PROTECT)
+    business_unit = models.ForeignKey(BusinessUnit, on_delete=models.PROTECT)
+    sub_business_unit = models.ForeignKey(SubBusinessUnit, on_delete=models.PROTECT, null=True, blank=True)
+    detail_business_unit = models.ForeignKey(DetailBusinessUnit, on_delete=models.PROTECT, null=True, blank=True)
+    branch_office = models.ForeignKey(BranchOffice, on_delete=models.PROTECT, null=True, blank=True)
+    account = models.ForeignKey(Account, on_delete=models.PROTECT, null=True, blank=True)
+    job_role = models.ForeignKey(JobRole, on_delete=models.PROTECT)
+
+    comments = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Self Owned"
+        verbose_name_plural = "Self Owned"
+
+        ordering = ("employee_id", "employee_name")
+
+    def __str__(self) -> str:
+        return self.employee_name
+
+class InternDatabase(models.Model):
+    STATUS_ON_DUTY = "o"
+    STATUS_REPLACING = "r"
+    STATUS_LEFT = "l"
+    STATUS_CHOICES = (
+        (STATUS_ON_DUTY, "Active"),
+        (STATUS_REPLACING, "Replacing"),
+        (STATUS_LEFT, "Inactive"),
+    )
+
+    GENDER_FEMALE = "F"
+    GENDER_MALE = "M"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+    )
+
+    intern_id = models.CharField(max_length=80, unique=True, verbose_name="Huawei ID")
+    intern_name = models.CharField(max_length=80, verbose_name="Name")
+
+    intern_email = models.EmailField(null=True, blank=True, verbose_name="Email")
+
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default=STATUS_ON_DUTY)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+
+    base_location = models.ForeignKey(ServiceLocation, on_delete=models.PROTECT)
+    department = models.ForeignKey(Department, on_delete=models.PROTECT)
+    business_unit = models.ForeignKey(BusinessUnit, on_delete=models.PROTECT)
+    sub_business_unit = models.ForeignKey(SubBusinessUnit, on_delete=models.PROTECT, null=True, blank=True)
+    detail_business_unit = models.ForeignKey(DetailBusinessUnit, on_delete=models.PROTECT, null=True, blank=True)
+    branch_office = models.ForeignKey(BranchOffice, on_delete=models.PROTECT, null=True, blank=True)
+    account = models.ForeignKey(Account, on_delete=models.PROTECT, null=True, blank=True)
+    job_role = models.ForeignKey(JobRole, on_delete=models.PROTECT, null=True, blank=True)
+
+    supervisor_id = models.CharField(max_length=80, null=True, blank=True)
+    supervisor_name = models.CharField(max_length=80, null=True, blank=True)
+
+    contract_start_date = models.DateField(verbose_name="Start date", null=True, blank=True) 
+    planned_contract_start_date = models.DateField(verbose_name="Planned start date", null=True, blank=True) 
+    contract_end_date = models.DateField(verbose_name="Contract end date", null=True, blank=True)
+    release_date = models.DateField(verbose_name="Release date", null=True, blank=True)
+    planned_release_date = models.DateField(verbose_name="Planned release date", null=True, blank=True)  
+
+    comments = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Intern"
+        verbose_name_plural = "Intern"
+
+        ordering = ("intern_id", "intern_name")
+
+    def __str__(self) -> str:
+        return self.intern_name
--- settings/models.py
+++ settings/models.py
@@ -1,32 +1,27 @@
+from _typeshed import OpenBinaryModeReading
 from django.db import models
 from django.db.models.manager import Manager
 
 from users.models import User
 
-# Create your models here.
 
-class BaseModel(models.Model):
-    name = models.CharField(max_length=40, blank=False, null=False)
-
-    active = models.BooleanField(default=True)
-
-    class Meta:
-        verbose_name = ""
-        verbose_name_plural = ""
-
-        ordering = ()
-
-class Account(BaseModel):
+class Account(models.Model):
    
     name = models.CharField(max_length=30, blank=False, null=False)
     manager = models.ForeignKey(User, on_delete=models.PROTECT, blank=False, null=False)
 
     active = models.BooleanField(default=True)
 
+    class Meta:
+        verbose_name = "Account"
+        verbose_name_plural = "Accounts"
+
+        ordering = ("name")
+
     def __str__(self) -> str:
         return self.name
 
-class Department(BaseModel):
+class Department(models.Model):
 
     name = models.CharField(max_length=30, blank=False, null=False)
     approver = models.ForeignKey(User, on_delete=models.PROTECT, blank=True, null=True)
@@ -34,8 +29,14 @@ class Department(BaseModel):
     active = models.BooleanField(default=True)
     use_bu_on_dashboard = models.BooleanField(default=False)
 
+    class Meta:
+        verbose_name = "Department"
+        verbose_name_plural = "Departments"
 
-class ResourceDepartment(BaseModel):
+        ordering = ("name")
+
+
+class ResourceDepartment(models.Model):
     
     name = models.CharField(max_length=30, blank=False, null=False)
 
@@ -46,3 +47,200 @@ class ResourceDepartment(BaseModel):
         verbose_name_plural = "Resource Departments"
 
         ordering = ("name")
+
+class BusinessUnit(models.Model):
+    name = models.CharField(max_length=40, blank=False, null=False)
+    department = models.ForeignKey(Department, blank=False, null=False, on_delete=models.PROTECT)
+    managers = models.ManyToManyField(User, blank=True, null=True)
+    allowed_users = models.ManyToManyField(User, blank=True, null=True)
+
+    active = models.BooleanField(default=True)
+    available_for_request = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "Business Unit"
+        verbose_name_plural = "Business Units"
+
+        ordering = ("name")
+
+    def __str__(self) -> str:
+        return self.name
+
+class SubBusinessUnit(models.Model):
+    business_unit = models.ForeignKey(BusinessUnit, on_delete=models.PROTECT, blank=False, null=False)
+    name = models.CharField(max_length=40, blank=False, null=False)
+
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "Sub Business Unit"
+        verbose_name = "Sub Business Units"
+
+        ordering = ("name")
+
+    def __str__(self) -> str:
+        return self.name
+
+class DetailBusinessUnit(models.Model):
+    name = models.CharField(max_length=40, blank=False, null=False)
+    sub_business_unit = models.ForeignKey(SubBusinessUnit, on_delete=models.PROTECT, blank=False, null=False)
+
+    active = models.BooleanField(default=True)
+
+    class Meta: 
+        verbose_name = "Detail Business Unit"
+        verbose_name_plural = "Detail Business Units"
+
+        ordering = ("name")
+
+    def __str__(self) -> str:
+        return self.name
+
+class ServiceType(models.Model):
+    name = models.CharField(max_length=40, blank=False, null=False)
+    business_unit = models.ForeignKey(BusinessUnit, on_delete=models.PROTECT, blank=False, null=False)
+
+    active = models.BooleanField(default=True)
+
+    class Meta: 
+        verbose_name = "Service Type"
+        verbose_name_plural = "Service Types"
+
+        ordering = ("name")
+
+    def __str__(self) -> str:
+        return self.name  
+
+class ServiceName(models.Model):
+    name = models.CharField(max_length=40, blank=False, null=False)
+    service_type = models.ForeignKey(ServiceType, on_delete=models.PROTECT, blank=False, null=False)
+
+    active = models.BooleanField(default=True)
+
+    class Meta: 
+        verbose_name = "Service Name"
+        verbose_name_plural = "Service Names"
+
+        ordering = ("name")
+
+    def __str__(self) -> str:
+        return self.name
+
+class ServiceLevel(models.Model):
+    name = models.CharField(max_length=40, blank=False, null=False)
+    service_name = models.ForeignKey(ServiceType, on_delete=models.PROTECT, blank=False, null=False)
+    sow = models.CharField(max_length=20, blank=False, null=False)
+
+    active = models.BooleanField(default=True)
+
+    class Meta: 
+        verbose_name = "Service Level"
+        verbose_name_plural = "Service Levels"
+
+        ordering = ("name")
+
+    def __str__(self) -> str:
+        return self.name
+
+class ServiceLocation(models.Model):
+    name = models.CharField(max_length=40, blank=False, null=False, unique=True)
+
+    available_for_requests = models.BooleanField(default=True)
+    active = models.BooleanField(default=True)
+
+    class Meta: 
+        verbose_name = "Service Location"
+        verbose_name_plural = "Service Locations"
+
+        ordering = ("name")
+
+    def __str__(self) -> str:
+        return self.name
+
+class BranchOffice(models.Model):
+    name = models.CharField(max_length=40, blank=False, null=False, unique=True)
+
+    active = models.BooleanField(default=True)
+
+    class Meta: 
+        verbose_name = "Branch Office"
+        verbose_name_plural = "Branch Offices"
+
+        ordering = ("name")
+
+    def __str__(self) -> str:
+        return self.name
+
+class JobRole(models.Model):
+    name = models.CharField(max_length=40, blank=False, null=False)
+
+    active = models.BooleanField(default=True)
+
+    class Meta: 
+        verbose_name = "Job Role"
+        verbose_name_plural = "Job Roles"
+
+        ordering = ("name")
+
+    def __str__(self) -> str:
+        return self.name
+
+class CostOwner(models.Model):
+    name = models.CharField(max_length=40, blank=False, null=False)
+
+    active = models.BooleanField(default=True)
+
+    class Meta: 
+        verbose_name = "Cost Owner"
+        verbose_name_plural = "Cost Owners"
+
+        ordering = ("name")
+
+    def __str__(self) -> str:
+        return self.name
+
+class HPCPName(models.Model):
+    name = models.CharField(max_length=40, blank=False, null=False)
+
+    active = models.BooleanField(default=True)
+
+    class Meta: 
+        verbose_name = "HPCP Name"
+        verbose_name_plural = "HPCP Names"
+
+        ordering = ("name")
+
+    def __str__(self) -> str:
+        return self.name
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=40, blank=False, null=False)
+    hpcp_name = models.ForeignKey(HPCPName, on_delete=models.PROTECT, blank=True, null=True)
+
+    active = models.BooleanField(default=True)
+
+    class Meta: 
+        verbose_name = "Supplier"
+        verbose_name_plural = "Suppliers"
+
+        ordering = ("name")
+
+    def __str__(self) -> str:
+        return self.name
+
+class Project(models.Model):
+    name = models.CharField(max_length=40, blank=False, null=False)
+    code = models.CharField(max_length=40, blank=False, null=False)
+
+    account = models.ForeignKey(Account, blank=False, null=False, on_delete=models.PROTECT)
+
+    active = models.BooleanField(default=True)
+
+    class Meta: 
+        verbose_name = "Project"
+        verbose_name_plural = "Projects"
+
+        ordering = ("name")
+
+    def __str__(self) -> str:
+        return self.name
